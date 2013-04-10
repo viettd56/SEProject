99481c562b7a82d76b8038c4315945c0c9d0a5ec
viettd56 <viet751993@gmail.com>
2013-04-10 09:04:00 +0700

    edit MonHoc, add unit test ThemMonHoc, XoaMonHoc

---
+++ SEProject/SEProject.Tests/Controllers/MonHocControllerTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Assert = NUnit.Framework.Assert;
+using SEProject.Controllers;
+using SEProject.Models;
+using System.Web.Mvc;
+
+namespace SEProject.Tests.Controllers
+{
+    [TestClass]
+    public class MonHocControllerTest
+    {
+        int id = 100;
+        string maMH = "INT123";
+        string tenMH = "Mon hoc 1";
+        int soTC = 2;
+        int soGLT = 20;
+        int soGTH = 15;
+        int soGT = 10;
+
+
+        [TestMethod]
+        public void ThemMonHoc()
+        {
+
+           
+            var monHoc = new MonHoc(id, maMH, tenMH, soTC, soGLT, soGTH, soGT, "");
+            
+
+            var _monHocController = new MonHocController();
+            var result = _monHocController.Create(monHoc) as RedirectToRouteResult;
+            Assert.NotNull(result);
+            Assert.AreEqual("Index", result.RouteValues["action"]);
+            }
+
+        [TestMethod]
+
+        public void XoaMonHoc()
+        {
+            int id = 10;
+            var _monHocController = new MonHocController();
+            var monHoc = new MonHoc(id, maMH, tenMH, soTC, soGLT, soGTH, soGT, "");
+            _monHocController.Create(monHoc);
+            var result = _monHocController.Delete(id) as ViewResult;
+            var mH = (MonHoc)result.ViewData.Model;
+            Assert.AreEqual("Mon hoc 1", mH.tenMonHoc);
+        }
+    }
+}
--- SEProject/SEProject/Models/MonHoc.cs
+++ SEProject/SEProject/Models/MonHoc.cs
@@ -16,6 +16,23 @@ namespace SEProject.Models
         public int gioThucHanh { get; set; }
         public int gioTuHoc { get; set; }
         public string maMonKienQuyet { get; set; }
+
+        public MonHoc()
+        {
+
+        }
+
+        public MonHoc(int id, string maMH, string tenMH, int soTC, int gioLT, int gioTHanh, int gioTHoc, string maMKQ)
+        {
+            Id = id;
+            maMonHoc = maMH;
+            tenMonHoc = tenMH;
+            soTinChi = soTC;
+            gioLyThuyet = gioLT;
+            gioThucHanh = gioTHanh;
+            gioTuHoc = gioTHoc;
+            maMonKienQuyet = maMKQ;
+        }
     }
 
     public class MonHocDBContext : DbContext

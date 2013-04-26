77c73e16c9a79e62c0f0aab3158f5b58975d5c07
thaimanhtuan <thaimanhtuan9319@gmail.com>
2013-04-26 09:24:33 +0700

    add Test CreatDaoTao

---
+++ SEProject/SEProject.Tests/Controllers/DaoTaoControllerTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SEProject.Models;
+using SEProject.Controllers;
+using System.Web.Mvc;
+using Assert = NUnit.Framework.Assert;
+
+namespace SEProject.Tests.Controllers
+{
+    [TestClass]
+    public class DaoTaoControlerTest
+    {
+        int ID = 10;
+        string tenGiangVien = "test";
+        string tenMonHoc = "test";
+
+        [TestMethod]
+        public void TestCreatDaoTao()
+        {
+            var daoTao = new DaoTao(tenMonHoc, tenGiangVien);
+            var _daoTaoControler = new DaoTaoController();
+            var result = _daoTaoControler.Create(tenMonHoc, tenGiangVien) as RedirectToRouteResult;
+            //Them
+            Assert.NotNull(result);
+            Assert.AreEqual("Index",result.RouteValues["action"]);
+
+            _daoTaoControler.Delete(daoTao.ID);
+            //Xoa
+        }
+    }
+}
